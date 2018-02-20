b03983692816fa9d5c98af509d309a06107af9c5
Jens Dede <github@jdede.de>
2018-02-20 19:04:43 +0100

    Several changes: * Close PHP Tags * Use Google Noto font * Show available display sizes if not given * fixed indention / mixing of spaces / tabs * Some minor improvements

--- server/contents/door_sign.php
+++ server/contents/door_sign.php
@@ -1,18 +1,20 @@
 <?php
-	checkFreeType();
-	// define your settings here
-	const ROOM = 'Raum 267';
-	const PERSONS = array("John Doe", "Jane Doe", "Otto Normalverbraucher");
-	
-	$fontSize = $scale;
-	
-	$cursorY += $fontSize*1.5;
+    checkFreeType();
+    // define your settings here
+    const ROOM = 'Raum 267';
+    const PERSONS = array("John Doe", "Jane Doe", "Otto Normalverbraucher");
+
+    $fontSize = $scale;
+
+    $cursorY += $fontSize*1.5;
     imagettftext($im, $fontSize, 0, 10, $cursorY, $red, $DEFAULT_FONT['bold'], ROOM);
-	$cursorY += 5;
-	imageline ($im , 10 , $cursorY , 1000 , $cursorY , $black );
-	
-	$fontSize = 0.5*$scale;	
-	foreach(PERSONS as $person){
-		$cursorY = $cursorY+$fontSize*1.5;
-	imagettftext($im, $fontSize, 0, 20, $cursorY, $black, $DEFAULT_FONT['regular'], $person );
-	}
+    $cursorY += 5;
+    imageline ($im , 10 , $cursorY , $displayWidth - 20, $cursorY , $black );
+
+    $fontSize = 0.5*$scale;
+    foreach(PERSONS as $person){
+        $cursorY = $cursorY+$fontSize*1.5;
+    imagettftext($im, $fontSize, 0, 20, $cursorY, $black, $DEFAULT_FONT['regular'], $person );
+    }
+
+?>
--- server/contents/static_image.php
+++ server/contents/static_image.php
@@ -1,37 +1,37 @@
 <?php
-	//Place your image in folder "static_image". This code will take a random image
-	$contents = scandir('contents/static_image');
-	if(!count($contents)){
-		exit;
-	}
-	foreach ($contents as $content) { 
-		$contentFile = pathinfo($content); 
-		
-		if($contentFile['extension'] == "png" OR $contentFile['extension'] == "jpg" OR $contentFile['extension'] == "jpeg"){
-			$someFile['ext'] = $contentFile['extension'];
-			$someFile['path'] = "contents/static_image/".$content;			
-			$allFiles[] = $someFile;
-		} 
-	}
-			
-		$content = $allFiles[array_rand($allFiles)];
-	
-		if($content['ext'] == "png"){
-			$imageSource = imagecreatefrompng($content['path']);
-			
-		}
-		if($content['ext'] == "jpg" OR $content['ext'] == "jpeg" ){
-			$imageSource = imagecreatefromjpeg($content['path']);
-			
-		}
-			
-		list($origW, $origH) = getimagesize($content['path']);
-		
+    //Place your image in folder "static_image". This code will take a random image
+    $contents = scandir('contents/static_image');
+    if(!count($contents)){
+        exit;
+    }
+    foreach ($contents as $content) {
+        $contentFile = pathinfo($content);
+
+        if($contentFile['extension'] == "png" OR $contentFile['extension'] == "jpg" OR $contentFile['extension'] == "jpeg"){
+            $someFile['ext'] = $contentFile['extension'];
+            $someFile['path'] = "contents/static_image/".$content;
+            $allFiles[] = $someFile;
+        }
+    }
+
+        $content = $allFiles[array_rand($allFiles)];
+
+        if($content['ext'] == "png"){
+            $imageSource = imagecreatefrompng($content['path']);
+
+        }
+        if($content['ext'] == "jpg" OR $content['ext'] == "jpeg" ){
+            $imageSource = imagecreatefromjpeg($content['path']);
+
+        }
+
+        list($origW, $origH) = getimagesize($content['path']);
+
     $width = $origW;
     $height = $origH;
-	
-	$maxW = $displayWidth ;
-	$maxH = $displayHeight;
+
+    $maxW = $displayWidth ;
+    $maxH = $displayHeight;
 
     if ($height > $maxH) {
         $width = ($maxH / $height) * $width;
@@ -42,6 +42,8 @@
         $height = ($maxW / $width) * $height;
         $width = $maxW;
     }
-	$imageNew = imagecreate($displayWidth, $displayHeight);
+    $imageNew = imagecreate($displayWidth, $displayHeight);
     imagecopyresampled($imageNew, $imageSource, 0, 0, 0, 0, $width, $height, $origW, $origH);
-	imagecopymerge($im, $imageNew, 0, 0, 0, 0, $displayWidth, $displayHeight,100);
+    imagecopymerge($im, $imageNew, 0, 0, 0, 0, $displayWidth, $displayHeight,100);
+    imagedestroy($imagenew);
+?>
