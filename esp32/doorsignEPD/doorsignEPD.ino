40e2664380007c5e8cf5a4317cd67a26fc13f8ec
Jens Dede <github@jdede.de>
2018-03-04 20:56:15 +0100

    Migrate to async TCP connection

--- esp32/doorsignEPD/doorsignEPD.ino
+++ esp32/doorsignEPD/doorsignEPD.ino
@@ -1,20 +1,26 @@
+#include <AsyncTCP.h>
+
 #define DEBUG 1
+
 #include <Basecamp.hpp>
 
 // Define your display type here: 2.9, 4.2 (bw and bwr) or 7.5 (bw or bwr) inches are supported:
 // Default: 4.2bwr
-#define DISPLAY_TYPE '4.2'
+#define DISPLAY_TYPE '4.2bwr'
 
-# Default 5
-#define CHIP_SELECT 15
+// Default 5
+#define CHIP_SELECT 5
 
-// Default -1 (deactivated)
-#define STATUS_PIN 5
+// Default -1: disabled
+#define STATUS_PIN -1
 
 #define FactorSeconds 1000000LL
 #define BASECAMP_NOMQTT
-
 Basecamp iot;
+AsyncClient client;
+bool tcpClientConnected = false;
+bool tcpClientConnectionInProgress = false;
+
 #include <GxEPD.h>
 
 #if DISPLAY_TYPE == '1.5'
@@ -53,7 +59,7 @@ String displayType = "7.5bwr";
 
 GxIO_Class io(SPI, CHIP_SELECT, 17, 16);
 GxEPD_Class display(io, 16, 4);
-int value = 0;
+
 bool connection = false;
 bool production = false;
 bool setupMode  = false;
@@ -129,11 +135,7 @@ void setup() {
         }
 
       }
-
-
-
     }
-
   } else {
     production = true;
     int retry = 0;
@@ -148,141 +150,215 @@ void setup() {
 
   }
 
+  // Create client
+  client.onConnect(&onConnectHandler);
+  client.onData(&onDataHandler);
+  client.onDisconnect(&onDisconnectHandler);
+  client.onTimeout(&onTimeoutHandler);
+  client.onError(&onErrorHandler);
 }
 
+/** Draw the pixels to the screen
+ *  
+ *  @param  char *data    A char array
+ *  @param  size_t len    Length of the char array
+ *  @param  boolean start True if the begin of a new screen
+ * 
+ */
+void drawPixels(char *data, size_t len, boolean start){
+  static int x;
+  static int y;
+  if (start){
+    x = 0;
+    y = 0;
+    display.eraseDisplay(true);
+  }
+
+  Serial.println(String("Printing ") + len + " Bytes to the screen");
+  for (size_t i=0; i<len; i++){
+
+    if (hasRed == true) {
+      for (int b = 7; b >= 0; b -= 2) {
+        int bit = bitRead(data[i], b);
+        int bit2 = bitRead(data[i], b - 1);
+
+        if ((bit == 1) && (bit2 == 1)) {
+          display.drawPixel(x, y, GxEPD_BLACK);
+        } else {
+          if ((bit == 0) && (bit2 == 1)) {
+            display.drawPixel(x, y, GxEPD_RED);
+          } else {
+            display.drawPixel(x, y, GxEPD_WHITE);
+          }
+        }
+        x++;
+
+        if  (x == GxEPD_WIDTH) {
+          y++;
+          x = 0;
+        }
+      }
+    } else {  // hasRead
+      for (int b = 7; b >= 0; b--) {
+        int bit = bitRead(data[i], b);
+        if (bit == 1) {
+          display.drawPixel(x, y, GxEPD_BLACK);
+        } else {
+          display.drawPixel(x, y, GxEPD_WHITE);
+        }
+        x++;
+        if  (x == GxEPD_WIDTH) {
+          y++;
+          x = 0;
+        }
+      }
+    }
+  }
+}
+
+/**
+ *  Handler called after connection is established
+ */
+void onConnectHandler(void *r, AsyncClient *client){
+  Serial.println("OnConnect\n");
+  tcpClientConnected = true;
+  tcpClientConnectionInProgress = false;
+  if (STATUS_PIN >= 0){
+    digitalWrite(5, HIGH);
+  }
+  String url =  iot.configuration.get("ImageAddress") + "&display=" + displayType;
+  String query = String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + iot.configuration.get("ImageHost") + "\r\n" +
+                 "Connection: close\r\n\r\n";
+
+  client->write(query.c_str());
+}
+
+/**
+ * Handler called for each received data packet
+ */
+void onDataHandler(void *r, AsyncClient *client, void *data, size_t len){ 
+  Serial.println(String("OnData: ") + len + " Bytes");
+  int16_t endHeader = findEndHeader((char*) data, len);
+
+  if (endHeader > 0){
+    // We got the header
+    char subbuf[endHeader + 1];
+    memcpy(subbuf, data, endHeader);
+    subbuf[endHeader] = 0;
+    String header = String(subbuf);
+    Serial.println(header);
+    
+    // handle header data
+    if (header.indexOf("X-productionMode: false") > 0) {
+      iot.configuration.set("ProductionMode", "false");
+      production = false;
+    }
+    if (header.indexOf("X-productionMode: true") > 0) {
+      iot.configuration.set("ProductionMode", "true");
+      production = true;
+    }
+    Serial.println(String("ProductionMode: ") + production);
+    
+    // Handle remaining data bytes. 4 bytes for \r\n\r\n separation
+    drawPixels((char*)data+endHeader+4, len-endHeader-4, true);
+  } else {
+    // No header -> directly draw to display
+    drawPixels((char*)data, len, false);
+  }
+}
+
+/**
+ * Handler called in case of a timeout
+ */
+void onTimeoutHandler(void *r, AsyncClient *client, uint32_t timeout){
+  Serial.println(String("Timeout ") + timeout);
+  transmitDone();
+}
+
+/**
+ * Handler called after a disconnect event
+ */
+void onDisconnectHandler(void *r, AsyncClient *client){
+  Serial.println("OnDisconnect");
+  display.update();
+  transmitDone();
+}
+
+/**
+ * Handler for the error cases
+ */
+void onErrorHandler(void *r, AsyncClient *client, int8_t error){
+  Serial.println(String("Error:") + error);
+  transmitDone();
+}
+
+/** Find the end to the HTTP header marked by "\r\n\r\n"
+ *  
+ *  @param char *buf    A char array
+ *  @param  size_t len  Length of the char array
+ *  @return The position of \r\n\r\n
+ */
+int16_t findEndHeader(char *buf, size_t len){
+  const char *endString = "\r\n\r\n";
+  for (int16_t i=0; i<len-4; i++){
+    if (
+      buf[i] == endString[0] &&
+      buf[i+1] == endString[1] &&
+      buf[i+2] == endString[2] &&
+      buf[i+3] == endString[3]
+      ) {
+        return i;
+      }
+  }
+  return -1;
+}
+
+/**
+ * Called after the transmission is finished (error, connection closed, timeout)
+ */
+void transmitDone(){
+  Serial.println("Transmit done");
+  if (STATUS_PIN >= 0){
+    digitalWrite(5, LOW);
+  }
+  tcpClientConnected = false;
+  tcpClientConnectionInProgress = false;
+}
+
+
+/**
+ * The main loop
+ */
 void loop() {
 
-  if (WiFi.status() == WL_CONNECTED) {
-    if (STATUS_PIN >= 0){
-      digitalWrite(5, HIGH);
-    }
-    String url =  iot.configuration.get("ImageAddress") + "&display=" + displayType;
-    boolean currentLineIsBlank = true;
-    WiFiClient client;
-    delay(5000);
-    ++value;
-
-    const int httpPort = 80;
-    const char* host = iot.configuration.get("ImageHost").c_str();
-
-    if (!client.connect(host, httpPort)) {
-      Serial.println("connection failed");
-      return;
+  if (WiFi.status() == WL_CONNECTED && !tcpClientConnected && !tcpClientConnectionInProgress) {
+      const int httpPort = 80;
+      const char* host = iot.configuration.get("ImageHost").c_str();
+      
+      if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        return;
+      } else {
+        Serial.println("Wait till the client is connected");
+      }    
     }
 
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + iot.configuration.get("ImageHost") + "\r\n" +
-                 "Connection: close\r\n\r\n");
-
-    int x = 0;
-    int y = 0;
-    int start = 0;
-    bool data = false;
-    String header;
-
-    display.eraseDisplay(true);
-
-    while (client.available()) {
-      char byte = client.read();
-
-      if (data == false) {
-        header +=  byte;
-      }
-
-      if (byte == '\n' && currentLineIsBlank && data == false) {
-        data = true;
-
-        if (header.indexOf("X-productionMode: false") > 0) {
-          iot.configuration.set("ProductionMode", "false");
-          production = false;
-          Serial.println("ProductionMode set to false by server");
-        }
-        if (header.indexOf("X-productionMode: true") > 0) {
-          iot.configuration.set("ProductionMode", "true");
-          production = true;
-          Serial.println("ProductionMode set to true by server");
-        }
-      }
-      if (byte == '\n' && data == false) {
-        currentLineIsBlank = true;
-      } else if (byte != '\r' && data == false) {
-        currentLineIsBlank = false;
-      }
-
-      if (data) {
-        if (start < 1) {
-          start++;
-        } else {
-
-
-          if (hasRed == true) {
-            for (int b = 7; b >= 0; b -= 2) {
-              int bit = bitRead(byte, b);
-              int bit2 = bitRead(byte, b - 1);
-
-              if ((bit == 1) && (bit2 == 1)) {
-                display.drawPixel(x, y, GxEPD_BLACK);
-              } else {
-                if ((bit == 0) && (bit2 == 1)) {
-                  display.drawPixel(x, y, GxEPD_RED);
-                } else {
-                  display.drawPixel(x, y, GxEPD_WHITE);
-                }
-              }
-              x++;
-
-              if  (x == GxEPD_WIDTH) {
-                y++;
-                x = 0;
-              }
-            }
-          } else {
-
-            for (int b = 7; b >= 0; b--) {
-              int bit = bitRead(byte, b);
-
-              if (bit == 1) {
-                display.drawPixel(x, y, GxEPD_BLACK);
-              } else {
-                display.drawPixel(x, y, GxEPD_WHITE);
-              }
-
-              x++;
-
-              if  (x == GxEPD_WIDTH) {
-                y++;
-                x = 0;
-              }
-            }
-          }
-
-        }
-
-      }
-    }
-    display.update();
-    Serial.println("Image loaded.");
-  }
+  Serial.println(String("STAT") + production + " / " + WiFi.status() != WL_CONNECTED + " / " + !setupMode);
+  
   if (
-      production == true ||               // Production mode
+      production ||                         // Production mode
       (
-        WiFi.status() != WL_CONNECTED &&  // Not connected to the WiFi network
-        setupMode == false                // Not in setup mode
+        WiFi.status() != WL_CONNECTED &&    // Not connected to the WiFi network
+        !setupMode                          // Not in setup mode
       )
     ) {
-
     int SleepTime = iot.configuration.get("ImageWait").toInt();
     esp_sleep_enable_timer_wakeup(FactorSeconds * (uint64_t)SleepTime);
-    if (STATUS_PIN >= 0){
-      digitalWrite(5, LOW);
-    }
     Serial.println("Going to sleep now...");
     esp_deep_sleep_start();
   } else {
-    if (STATUS_PIN >= 0) {
-      digitalWrite(5, LOW);
-    }
-    delay(5000);
+    delay(20000);
     Serial.println("Setup: Not going to sleep. Use web config to setup.");
   }
 };
