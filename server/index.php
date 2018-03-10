fce502e114c88a964cc4bc7c82f2e41e1e5a7584
Jens Dede <github@jdede.de>
2018-03-11 00:38:21 +0100

    Allow setting the sleep times dynamically via an HTTP-header by the server, solves #11

--- server/index.php
+++ server/index.php
@@ -4,6 +4,9 @@
 //To stop productionMode (no deep sleep, web config), set http-header X-productionMode: false
 #header("X-productionMode: false");
 
+// Set the sleep interval for the doorsigns via the server
+#header("X-sleepInterval: 60 ");
+
 error_reporting('E_ERROR');
 # Supported displays:
 # 1.54 inches: https://www.waveshare.com/wiki/1.54inch_e-Paper_Module
@@ -64,12 +67,7 @@ if(strlen($_GET['scale']) AND is_numeric($_GET['scale'])){
 
 $displayType = $_GET['display'];
 if(!isset(DISPLAYS[$displayType])){
-    echo ("Not a valid display size. <br />");
-    echo ("display=[");
-    foreach (array_keys(DISPLAYS) as $display_key){
-        echo ($display_key.", ");
-    }
-    echo ("]");
+    echo ("Not a valid display size.");
     exit;
 }
 
