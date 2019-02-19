2a9055ad0ec771d9f0645573a0e4b4854301d70d
ger82 <ger82@users.noreply.github.com>
2019-02-19 23:01:00 +0100

    Update ical_calendar.php
    add coments
    replace german variable names with english ones

--- server/contents/ical_calendar.php
+++ server/contents/ical_calendar.php
@@ -11,48 +11,69 @@
 	
 	$fontSize = 0.4*$scale;
 	
-	// http://sabre.io/vobject/
-	// https://github.com/sabre-io/vobject
+	/**
+	 *  http://sabre.io/vobject/
+	 *  https://github.com/sabre-io/vobject
+	 *  Here you can find the vobject Library. This is necessary to process the ical calendar.
+	 *  https://evertpot.com/icalendar-vcard-parser-for-php/
+	 *  There you can learn how vobject works.
+	 **/
 	require 'vobject/vendor/autoload.php';
 	use
 		Sabre\VObject;
 
 	$calData = [];
 
-	// https://username:passwort@mydomain.net/owncloud/remote.php/dav/calendars/username/personal?export
-	// http://evertpot.com/resources/files/posts/icalendartest.ics
+	/**
+	 * https://username:passwort@mydomain.net/owncloud/remote.php/dav/calendars/username/personal?export
+	 * This example show you how to import a calendar form owncloud with name "personal"
+	 * You must replace username and passwort with real data
+	 * http://evertpot.com/resources/files/posts/icalendartest.ics
+	 * This is an example from the former maintainer of the sabre project.
+	 *
+	 * Here you can insert more then one calendar / ical file
+	 **/
 	$calendarAdress = ['icalendartest.ics'];
 
 	foreach($calendarAdress as $adress) {
 		$calendar = VObject\Reader::read( file_get_contents($adress) );
 
-		$heute = new DateTime('2010-11-20');
-		$heute->setTime(00, 00);
-		$kommendeTage = new DateTime('+7 days');
-		$newVCalendar = $calendar->expand( $heute, $kommendeTage );
+		$today = new DateTime('2010-11-20');
+		$today->setTime(00, 00);
+		$nextWeek = new DateTime('+7 days');
+		$newVCalendar = $calendar->expand( $today, $nextWeek );
 		
 		if(!empty($newVCalendar->vevent)){
 			foreach($newVCalendar->vevent as $event) {
 
-				// https://web.archive.org/web/20160403000844/http://www.paulund.co.uk:80/datetime-php
+				/**
+				 *  This Website is helpful for working with DateTime
+				 *  https://web.archive.org/web/20160403000844/http://www.paulund.co.uk:80/datetime-php
+				 **/
 				$dtstart = new DateTime( (string)$event->dtstart );
 				
 				if( !empty( $event->dtend ) ){
 					$dtend = new DateTime( (string)$event->dtend );
-					$tageDifferenz = $dtstart->diff( $dtend );
+					$dayDifference = $dtstart->diff( $dtend );
 				}
-				
-				if( empty( $tageDifferenz->days ) ){
-					$iEnde = 1;
+				/**
+				 *  A distinction is made here between single and multi-day events.
+				 **/
+				if( empty( $dayDifference->days ) ){
+					$iEnd = 1;
 				}else{
-					$iEnde = $tageDifferenz->days;
+					$iEnd = $dayDifference->days;
 				}
 				
-				for($i = 1; $i <= $iEnde; $i++){
+				for($i = 1; $i <= $iEnd; $i++){
+					/**
+					 *  The if and the date adjustment are necessary so that
+					 *  events lasting several days are not only displayed on the first day.
+					 **/
 					if($i > 1){
 						$dtstart->modify('+1 day');
 					}
-					if($dtstart->getTimestamp() >= $heute->getTimestamp() ){
+					if($dtstart->getTimestamp() >= $today->getTimestamp() ){
 						$calData[] = [ (string)$dtstart->format('d-m-Y H:i'), (string)$event->summary ];
 					}
 				}	
@@ -61,16 +82,22 @@
 		}
 	}
 
-	// https://secure.php.net/manual/de/function.array-multisort.php
+	/**
+	 * Here you you can find more information about multisort
+	 * https://secure.php.net/manual/de/function.array-multisort.php
+	 **/
 	$ord = array();
 	foreach ($calData as $key => $value){
 		$ord[] = strtotime($value['0']);
 	}
 	array_multisort($ord, SORT_ASC, $calData);
 
+	/**
+	 * This array contains the individual weekday names in German.
+	 **/
 	$weekDaysNameFull = array("Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Sonnabend");
 
-	$datum = "";
+	$varDate = "";
 	$i = 1;
 	foreach($calData as $event) {
 		
@@ -80,14 +107,16 @@
 		
 		$datetime = new DateTime($event[0]);
 		
-		if( empty($datum) || $datum != $datetime->format('d-m-Y') ){
+		if( empty($varDate) || $varDate != $datetime->format('d-m-Y') ){
 			$cursorX += $fontSize * 1.5;
-			$datum = $datetime->format('d-m-Y');
+			$varDate = $datetime->format('d-m-Y');
 			$valueText = $weekDaysNameFull[ $datetime->format('w') ] . "  " . $datetime->format('d.m.Y');
 			imagettftext( $im, $fontSize, 90, $cursorX, 370, $black, $DEFAULT_FONT[ 'bold' ],  $valueText );
 			$cursorX += $fontSize * 1.5;
 		}
-		
+		/**
+		 * This is for example for birthdays which should be displayed all day.
+		 */
 		if($datetime->format('H:i') == "00:00"){ 
 			imagettftext( $im, $fontSize, 90, $cursorX, 370, $black, $DEFAULT_FONT[ 'regular' ], $event[1] );
 		}else{
